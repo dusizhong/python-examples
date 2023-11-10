e0b1fbfbb247d9186cf50cd64a65e11740887af9
unknown <5663314@qq.com>
2023-11-10 17:37:25 +0800

    add tangnet.py spider

--- spider/tangnet.py
+++ spider/tangnet.py
@@ -4,15 +4,15 @@
 import urllib.request, urllib.error
 from bs4 import BeautifulSoup
 import re
-import xlwt
-import time
 import random
+import time
+import xlwt
 
-url = 'http://www.tangnet.cn/novel/63.html#catalog'
+baseurl = 'http://www.tangnet.cn'
 headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36' }
-req = urllib.request.Request(url, headers=headers)
 
 try:
+	req = urllib.request.Request(baseurl + '/novel/63.html#catalog', headers=headers)
 	resp = urllib.request.urlopen(req, timeout=10.01)
 	print('response code: %s' % (resp.getcode()))
 	# print("response header:\n", resp.getheaders())
@@ -36,16 +36,19 @@ try:
 	for item in contents:
 		item = str(item)
 		links = re.findall(findLink, item)
+		print(len(links))
 		for l in links:
 			print(l)
-		# if len(link) > 0:
-		# 	print(link)
-		# if str(item).startswith('<div'):
-		# 	print(item)
-		# 	dataList.append(item)
-		# 	print(item)
-		# sleepTime = random.randint(2, 5)
-		# time.sleep(sleepTime)
+			req1 = urllib.request.Request(baseurl + l, headers=headers)
+			resp1 = urllib.request.urlopen(req1, timeout=10.01)
+			html1 = resp1.read().decode('utf-8')
+			soup1 = BeautifulSoup(html1, features='lxml')
+			title1 = soup1.select('.j_chapterName')[0].text
+			print(title1)
+			content1 = soup1.select('.read-content')[0].text
+			print(content1)
+			sleepTime = random.randint(2, 5)
+			time.sleep(sleepTime)
 	# print(dataList)
 
 except urllib.error.URLError as e:
